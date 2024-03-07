7bde2165afdba029565c2ec64070b13b3367b1d6
mikamttr <mika348@live.fr>
2024-03-07 09:44:51 +0100

    first commit

---
+++ src/test/kotlin/com/example/gamedayapi/GamedayApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.gamedayapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GamedayApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
